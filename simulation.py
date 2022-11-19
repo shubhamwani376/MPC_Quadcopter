96fe529133ffd2ae163d5ded87ba36eebbd77b3a
nikunjsanghai <103468688+nikunjsanghai@users.noreply.github.com>
2022-11-19 01:14:40 -0800

    updated trajectory generation

--- simulation.py
+++ simulation.py
@@ -1,24 +1,33 @@
-from matplotlib import pyplot as plt
 import numpy as np
-import networkx as nx
-import pathplanning as path
-import plot3denv as p3denv
+# from matplotlib import pyplot as plt
+# import networkx as nx
+# import plot3denv as p3denv
+import trajplan as tp
 
 if __name__ == '__main__':
-    # Total Arena 10x10x5
-    X = np.array([1,1,1])
-    X_goal = np.array([9,9,4])
-    arena_size = np.array([10,10,5])
-    X_goal_threshold = 0.1 # metre, sphere
-    path_threshold = np.array([0.1])
-    path_threshold = np.array([0.1])
+    X = np.array([1,1,1],dtype = float) # m,m,m
+    X_wp = np.array([5,9,2.5],dtype = float) # m,m,m
+    X_goal = np.array([9,1,4],dtype = float) # m,m,m
+    arena_size = np.array([10,10,5])  # m,m,m
+    tf1 = 30 # sec
+    tf2 = 30 # sec
+    time = 1
+    coeff = tp.trajpp(X,X_wp,X_goal,tf1,tf2)
+    goal = tp.postime(coeff,time,tf1,tf2)
+    print(goal)
+    #time = np.linspace
+
+    #for t in np.linspace
+
+
+
+    # X_goal_threshold = 0.1 # metre, sphere
+    # path_threshold = np.array([0.1])
+    # path_threshold = np.array([0.1])
 
     # Performing RRT* on env and getting the waypoints
-    path_rrt = np.array([])
-    path_rrt = path.rrt_star(X, X_goal, arena_size,  X_goal_threshold, path_threshold)
+    # path_rrt = np.array([])
+    # path_rrt = path.rrt_star(X, X_goal, arena_size,  X_goal_threshold, path_threshold)
 
     # Visualise the path
     #p3denv.
-
-
-
---
+++ trajplan.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def trajpp(X,X_wp,X_goal,tf1,tf2):
+    coeff = np.zeros((6,6))
+    for i in range(3):
+        coeff[:,i] = polycoeff(X[i],X_wp[i],tf1)
+    for j in range(3):
+        coeff[:,j+3] = polycoeff(X_wp[j],X_goal[j],tf2)
+    np.set_printoptions(precision=3)
+    print(coeff)
+    return coeff
+
+def polycoeff(pos_start,pos_end,tf):
+    bound = np.array([pos_start,pos_end,0,0,0,0],dtype = float)
+    mat = np.array([[0,0,0,0,0,1],[tf**5,tf**4,tf**3,tf**2,tf**1,1],[0,0,0,0,1,0],[5*tf**4,4*tf**3,3*tf**2,2*tf,1,0],[0,0,0,2,0,0],[20*tf**3,12*tf**2,6*tf,2,0,0]],dtype = float)
+    coeff_sub = np.matmul(np.linalg.inv(mat) , bound)
+    return coeff_sub
+
+def postime(coeff,time,tf1,tf2):
+    #pos = np.array([0,0,0],dtype = float) # m,m,m
+    if time <= tf1:
+        # 1st wala poly
+        timearray = np.array([time**5,time**4,time**3,time**2,time**1,1])
+        pos = np.multiply(timearray,coeff[:,0:3].T)
+        pos = np.sum([pos],axis = 2)
+    else:
+        time = time - tf1
+        timearray = np.array([time**5,time**4,time**3,time**2,time**1,1])
+        pos = print("second")
+        pos = np.multiply(timearray,coeff[:,3:6].T)
+        print(pos)
+        pos = np.sum([pos],axis = 2)
+    return pos #x,y,z
